a27703dc644b398c96be53166d334bfcc2992e99
reshmavikram <reshmavikramraj@gmail.com>
2022-03-10 12:20:28 +0530

    Add project files.

---
+++ usingentity/Data/StudentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using usingentity.Model;
+
+namespace usingentity.Data
+{
+    public class StudentDbContext:DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) :base(options) 
+        { 
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}  
---
+++ usingentity/Model/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace usingentity.Model
+{
+    public class Student
+    {
+        [Key]
+        public int id { get; set; }
+        public string Name { get; set; }
+        public int Mark { get; set; }
+
+
+    }
+}
